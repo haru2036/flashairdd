517830786666a2edaf38d470cd8e1fee756b25b3
haru2036 <haru2036@gmail.com>
2018-07-09 22:45:15 +0900

    add web wrapper

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
+import Web.Scotty
+import Data.Maybe
+import Control.Monad.IO.Class
+import Control.Concurrent.MVar
+import Control.Concurrent
+
 
 main :: IO ()
-main = someFunc
+main = do
+  downloadThread <- newMVar Nothing
+  scotty 3000 $ do
+    get "/download" $ do
+      downloading <- liftIO $ takeMVar downloadThread
+      liftIO $ case downloading of
+        Just threadId -> killThread threadId
+        Nothing -> return ()
+      liftIO $ do
+        threadId <- forkIO someFunc 
+        putMVar downloadThread $ Just threadId
+      html $ mconcat ["<h1>download started</h1>"]
+
+    get "/cancel" $ do
+      html $ mconcat ["<h1>canceling download</h1>"]
+      liftIO $ do
+        downloading <- takeMVar downloadThread
+        case downloading of
+          Just threadId -> killThread threadId
+          Nothing -> return ()
+        putMVar downloadThread $ Nothing
